48916cc58c001a82d3b9f469d0d6841cbf131671
Victor Carpes <victorcarpes@outlook.com>
2023-09-11 09:23:26 +0100

    Method get w/ @Path variable to find post by id

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,4 +24,13 @@ public class CodeblogController {
         mv.addObject("posts", posts);
         return mv;
     }
+
+    @RequestMapping (value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv =  new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post);
+        return mv;
+    }
+
 }
